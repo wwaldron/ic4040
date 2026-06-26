a53143ec18d7c467585c7c3f65f6a5543e1538f5
Will Waldron <wwaldron@harding.edu>
2026-06-26 14:24:07 -0500

    Updated CRDS updater

--- Images/update_crds.sh
+++ Images/update_crds.sh
@@ -4,4 +4,5 @@
 
 # Update the References
 # This assumes that the CRDS_SERVER_URL & CRDS_PATH environment variables have been set
-crds bestrefs --files RawImages/wfc3/*/*fl?.fits --sync-references=1 --update-bestrefs
+crds bestrefs --files RawImages/wfc3/Parallel/*/*fl?.fits --sync-references=1 --update-bestrefs
+crds bestrefs --files RawImages/wfc3/SBC/*/*fl?.fits --sync-references=1 --update-bestrefs
