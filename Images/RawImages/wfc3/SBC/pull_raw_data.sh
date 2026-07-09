13019314ee0964400b69101a1df963da96d7b764
Will Waldron <wwaldron@harding.edu>
2026-07-09 16:20:18 -0500

    Updated raw pull permission setting

--- Images/RawImages/wfc3/SBC/pull_raw_data.sh
+++ Images/RawImages/wfc3/SBC/pull_raw_data.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 rsync -avz --include='*/' --include='j*flt.fits' --exclude='*' /data/share_data/IC4040_SBC/visit_0* ./
+find visit_* -type f -exec chmod 644 {} +
+find visit_* -type d -exec chmod 755 {} +
